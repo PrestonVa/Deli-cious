a9424020adaa9b96247d47d26a303c82019f3e79
PrestonVa <166422855+PrestonVa@users.noreply.github.com>
2024-05-31 07:24:05 -0400

    Add files via upload

---
+++ Drink.java
@@ -0,0 +1,28 @@
+package com.pluralsight;
+
+class Drink {
+    // Create variables, as private.
+    private String size;
+    private String type;
+    private double price;
+
+    // Create the constructor.
+    public Drink(String size, String type, double price) {
+        this.size = size;
+        this.type = type;
+        this.price = price;
+    }
+
+    // Create getters.
+    public String getSize() {
+        return size;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
